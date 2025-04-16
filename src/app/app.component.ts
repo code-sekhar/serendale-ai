06d9b0208958496f381c12ef0701c29fcee91324
code-sekhar <sekharchaudhary5@gmail.com>
2025-04-16 07:41:03 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ScreeningComponent} from './pages/screening/screening.component';
+import {HeaderComponent} from './include/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ScreeningComponent,HeaderComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ScreeningComponent } from './pages/screening/screening.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: ScreeningComponent
+  }
+];
