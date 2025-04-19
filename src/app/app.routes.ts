099a636b225d89dfe6e2832461fe4c81de047312
sekhar chaudhaudhary <sekharchaudhary5@gmail.com>
2025-04-20 04:18:39 +0530

    feat: add all

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { ScreeningComponent } from './pages/screening/screening.component';
+import { Screening2Component } from './pages/screening2/screening2.component';
+import { SectorsIndustriesComponent } from './pages/sectors-industries/sectors-industries.component';
 
 export const routes: Routes = [
   {
     path: '', component: ScreeningComponent
+  },
+  {
+    path: 'screening2', component: Screening2Component
+  },
+  {
+    path: 'sectors-industries', component: SectorsIndustriesComponent
   }
 ];
--- src/app/pages/screening/screening.component.ts
+++ src/app/pages/screening/screening.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-screening',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './screening.component.html',
   standalone: true,
   styleUrl: './screening.component.scss'
